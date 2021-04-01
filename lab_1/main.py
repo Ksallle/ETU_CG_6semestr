451df190e28b12675c1945677944dada0bc6293c
Aleksandr Shvetsov <shveczov.s@inbox.ru>
2021-04-02 00:09:12 +0300

    add lab_1, lab_2

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,12 +1,96 @@
+import sys
 import matplotlib.pyplot as plt
-import numpy as np
 
-fig = plt.figure()
-print(fig.axes)
-print(type(fig))
 
-plt.scatter(1.0, 1.0)
+def matrixMulp(matrix_a, matrix_b):
+    w, h = len(matrix_b[0]), len(matrix_a)
+    result = [[0 for x in range(w)] for y in range(h)]
+    for i in range(len(matrix_a)):
+        for j in range(len(matrix_b[0])):
+            for k in range(len(matrix_b)):
+                result[i][j] += matrix_a[i][k] * matrix_b[k][j]
+    return result
 
-print(fig.axes)
 
-plt.show()
+points = [[float(point) for point in [*input(f'Координаты точки {i}: ').split(','), 1]]
+          for i in range(1, int(input("Введите количество точек: ")) + 1)]
+
+
+command = 0
+
+
+while command != 4:
+    print("Нажмите <1>, чтобы вывести изображение фигуры\nНажмите <2>, чтобы отразить фигуру\n"
+          "Нажмите <3>, чтобы задать точки для новой фигуры\nНажмите <4>, чтобы выйти")
+    command = int(input(">"))
+    if command == 1:
+        x = [p[0] for p in points]
+        y = [p[1] for p in points]
+        z = [p[2] for p in points]
+        temp = [x, y, z]
+        max_length = max(max(temp))
+        OX = [0, max_length * 1.25]
+        OY = [0, max_length * 1.25]
+        OZ = [0, max_length * 1.25]
+        null_arr = [0, 0]
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(x, y, z, label='parametric curve')
+        ax.plot(OX, null_arr, null_arr, label='parametric curve', alpha=0.3)
+        ax.plot(null_arr, OY, null_arr, label='parametric curve', alpha=0.3)
+        ax.plot(null_arr, null_arr, OZ, label='parametric curve', alpha=0.3)
+        ax.legend(['Исходная фигура', 'Ось X', 'Ось Y', 'Ось Z'])
+        plt.show()
+
+    elif command == 2:
+        T = [[1, 0, 0, 0],
+             [0, 1, 0, 0],
+             [0, 0, 1, 0],
+             [0, 0, 0, 1]]
+        print("Отразить фигуру относительно плоскости XOY? 1 - да 0 - нет")
+        flag = int(input(">"))
+        if flag == 1:
+            T[2][2] = -1
+        flag = 0
+        print("Отразить фигуру относительно плоскости YOZ? 1 - да 0 - нет")
+        flag = int(input(">"))
+        if flag == 1:
+            T[0][0] = -1
+        flag = 0
+        print("Отразить фигуру относительно плоскости ZOX? 1 - да 0 - нет")
+        flag = int(input(">"))
+        if flag == 1:
+            T[1][1] = -1
+        flag = 0
+        x = [p[0] for p in points]
+        y = [p[1] for p in points]
+        z = [p[2] for p in points]
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(x, y, z, label='parametric curve')
+        mirror = matrixMulp(points, T)
+        x1 = [p[0] for p in mirror]
+        y1 = [p[1] for p in mirror]
+        z1 = [p[2] for p in mirror]
+        ax.plot(x1, y1, z1, label='parametric curve', color='orange')
+        temp = [x, y, z, x1, y1, z1]
+        max_length = max(max(temp))
+        OX = [0, max_length * 1.25]
+        OY = [0, max_length * 1.25]
+        OZ = [0, max_length * 1.25]
+        null_arr = [0, 0]
+        ax.plot(OX, null_arr, null_arr, label='parametric curve', alpha=0.3)
+        ax.plot(null_arr, OY, null_arr, label='parametric curve', alpha=0.3)
+        ax.plot(null_arr, null_arr, OZ, label='parametric curve', alpha=0.3)
+        ax.legend(['Исходная фигура', 'Отраженная фигура',
+                  'Ось X', 'Ось Y', 'Ось Z'])
+        ax.set_box_aspect([1, 1, 1])
+        plt.show()
+
+    elif command == 3:
+        points = [[float(point) for point in [*input(f'Кординаты точки {i}: ').split(
+            ','), 1]] for i in range(1, int(input("Введите количество точек: ")) + 1)]
+    elif command == 4:
+        sys.exit()
+    else:
+        print("Неверная команда")
---
+++ lab_2/main.py
@@ -0,0 +1,144 @@
+import matplotlib.pyplot as plt
+import math
+import random
+
+
+def getBasis(i, t, n):
+    result = (math.factorial(n - 1)/(math.factorial(i)*math.factorial(n - 1 - i))) * pow(t, i) * pow(1 - t, n - 1 - i)
+    return result
+
+
+def bezier(points, n):
+    result_points = []
+
+    step = 0.01
+    if n <= 4:
+        t = 0
+
+        while t <= 1:
+            x = y = 0
+
+            for i in range(n):
+                temp = getBasis(i, t, n)
+                x += points[i][0] * temp
+                y += points[i][1] * temp
+            result_points.append(list([x, y]))
+            t += step
+
+    else:
+        num_points = 0
+        for i in range(0, len(points)):
+            num_points += 1
+            if num_points % 4 == 0:
+                mid_coord_x = (points[i - 1][0] + points[i][0])/2
+                mid_coord_y = (points[i - 1][1] + points[i][1]) / 2
+                temp = points[i]
+                points[i] = list([mid_coord_x, mid_coord_y])
+                points.insert(i+1, temp)
+                num_points += 1
+
+        k = math.ceil(num_points / 4)
+        num = 0
+        it = 0
+        minus = -1
+        for z in range(0, k):
+            t = 0
+
+            num += 4
+            it += 1
+            minus += 1
+            if z == k - 1 and num_points < k * 4:
+                while t <= 1:
+                    x = y = 0
+                    j = -1
+                    for i in range(num - 4 - minus, num - (k*4 - num_points)):
+                        j += 1
+                        temp = getBasis(j, t, num - (k*4 - num_points) - (num - 4 - minus))
+                        x += points[i][0] * temp
+                        y += points[i][1] * temp
+                    result_points.append(list([x, y]))
+                    t += step
+
+            else:
+                while t <= 1:
+                    x = y = 0
+
+                    j = -1
+                    for i in range(num - 4 - minus, num - minus):
+                        j += 1
+                        temp = getBasis(j, t, 4)
+                        x += points[i][0] * temp
+                        y += points[i][1] * temp
+                    result_points.append(list([x, y]))
+                    t += step
+
+    return result_points
+
+
+def main():
+    points = []
+    num_points = 0
+    ax = plt.axes()
+    while True:
+        print('Введите "1", чтобы ввести координаты точек')
+        print('Введите "2", чтобы изменить координаты точек')
+        print('Введите "3", чтобы вывести график')
+        print('Введите "0", чтобы выйти из программы')
+        command = int(input('> '))
+
+        if command == 2:
+            if len(points) == 0:
+                print('Вы еще не ввели координаты точек, поэтому невозможно изменить их')
+            else:
+                while True:
+                    print('Координаты какой точки вы хотите изменить?')
+                    print('Введите 0, если хотите выйти из пункта изменения координат')
+                    num_point = int(input('> '))
+                    if num_point == 0:
+                        break
+                    while num_point > num_points:
+                        print('Данной точки не существует, введите другую')
+                        num_point = int(input('> '))
+                    print('Старые координаты точки', num_point, ': ', points[num_point-1])
+                    point = list(map(int, input(f'Новые координаты точки {num_point}: ').split(',')))
+                    points[num_point-1] = point
+                result_points = bezier(points.copy(), n)
+
+        elif command == 1:
+            n = int(input('Введите количество точек: '))
+            print('Введите "1", если хотите сами задать координаты точек')
+            print('Введите "2", если хотите, чтобы координаты точек выбрались случайно')
+            print('Введите "0", чтобы выйти из программы')
+            command_1 = int(input('> '))
+            if command_1 == 1:
+                for i in range(0, n):
+                    point = list(map(int, input(f'Координаты {i+1} точки: ').split(',')))
+                    points.append(point)
+                num_points = len(points)
+                result_points = bezier(points.copy(), n)
+
+            elif command_1 == 2:
+                for i in range(0, n):
+                    point = list([random.randint(0, 20), random.randint(0, 10)])
+                    points.append(point)
+                num_points = len(points)
+                result_points = bezier(points.copy(), n)
+            elif command_1 == 3:
+                quit()
+
+        elif command == 3:
+            if len(points) == 0:
+                print('Вы еще не ввели ни одной точки\n')
+            else:
+                plt.plot(*zip(*points))[0]
+                plt.plot(*zip(*result_points))[0]
+                ax.legend(['Заданая ломаная', 'Составная кривая Безье'])
+                ax.set_xlabel('X')
+                ax.set_ylabel('Y')
+                plt.show()
+
+        elif command == 0:
+            quit()
+
+
+main()
